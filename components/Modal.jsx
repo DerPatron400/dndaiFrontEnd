f1e5fd7069bc4d0bafd8ff474eb7be53e81dc887
Aimen Elahi <aimenqaiser2000@gmail.com>
2023-12-05 09:57:53 -0800

    fixed audio

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -9,56 +9,56 @@ export default function EnableSound({ setPlaying }) {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
-        as='div'
-        className='relative z-1'
+        as="div"
+        className="relative z-1"
         initialFocus={cancelButtonRef}
         onClose={setOpen}
       >
         <Transition.Child
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300 transition-all"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 transition-opacity' />
+          <div className="fixed inset-0 transition-opacity" />
         </Transition.Child>
 
-        <div className='fixed inset-0 z-10 w-screen overflow-y-auto '>
-          <div className='flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0'>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto ">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-              enterTo='opacity-100 translate-y-0 sm:scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className='relative border-green-500 border transform overflow-hidden rounded-lg bg-black text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-                <div className='bg-black px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
-                  <div className='sm:flex sm:items-start'>
-                    <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+              <Dialog.Panel className="relative border-green-500 border transform overflow-hidden rounded-lg bg-black text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-black px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
-                        as='h3'
-                        className='text-base font-semibold leading-6 text-white'
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-white"
                       >
                         Enable Sound
                       </Dialog.Title>
-                      <div className='mt-2'>
-                        <p className='text-sm text-gray-500'>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
                           Do you want to turn on sound ?
                         </p>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className='bg-black px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
+                <div className="bg-black px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
-                    type='button'
-                    className='inline-flex w-full justify-center  border-0 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto'
+                    type="button"
+                    className="inline-flex w-full justify-center  border-0 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
                     onClick={() => {
                       setPlaying(true);
                       setOpen(false);
@@ -67,8 +67,8 @@ export default function EnableSound({ setPlaying }) {
                     Enable
                   </button>
                   <button
-                    type='button'
-                    className='mt-3 inline-flex w-full justify-center rounded-md border-0 bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto'
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border-0 bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     onClick={() => {
                       setPlaying(false);
                       setOpen(false);
