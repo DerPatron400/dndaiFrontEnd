b0db75354247c2d651b64223d68a505ac7703f12
Aimen Elahi <aimenqaiser2000@gmail.com>
2023-12-28 05:02:42 -0800

    resolved conflicts

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
   const { user } = useUserStore((state) => state);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
+  //settig sound
   const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
@@ -34,23 +35,23 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className='flex py-8 p-4 z-10 absolute top-0 left-0 w-[100%] h-[8vh] bg-black items-center justify-between'>
+    <div className="flex py-8 p-4 z-10 absolute top-0 left-0 w-[100%] h-[8vh] bg-black items-center justify-between">
       <EnableSound setPlaying={setPlaying} />
 
-      <div className='flex items-center justify-between w-full'>
-        <Link href='/' className='cursor-pointer hover:bg-transparent'>
-          <img src='/Logo/white.png' alt='Logo' className='h-16 w-16' />
+      <div className="flex items-center justify-between w-full">
+        <Link href="/" className="cursor-pointer hover:bg-transparent">
+          <img src="/Logo/white.png" alt="Logo" className="h-16 w-16" />
         </Link>
 
-        <div className='flex items-center space-x-4 pr-2'>
+        <div className="flex items-center space-x-4 pr-2">
           <div
             onClick={() => {
               setPlaying(!playing);
             }}
-            className='rounded-full bg-white p-2 cursor-pointer'
+            className="rounded-full bg-white p-2 cursor-pointer"
           >
             {!playing ? (
-              <VolumeX size={20} color='black' />
+              <VolumeX size={20} color="black" />
             ) : (
               <Volume2 size={20} />
             )}
@@ -61,14 +62,14 @@ const Navbar = () => {
             <div>
               {" "}
               <Link
-                href='/login'
-                className='cursor-pointer text-md text-black bg-white hover:bg-white hover:text-black px-4 py-1 rounded-md focus:outline-none transition-all duration-300 ease-in-out'
+                href="/login"
+                className="cursor-pointer text-md text-black bg-white hover:bg-white hover:text-black px-4 py-1 rounded-md focus:outline-none transition-all duration-300 ease-in-out"
               >
                 Login
               </Link>
               <Link
-                href='/register'
-                className='cursor-pointer text-md text-white bg-green-500 hover:bg-green-600 hover:text-white px-4 py-1 rounded-md focus:outline-none transition-all duration-300 ease-in-out'
+                href="/register"
+                className="cursor-pointer text-md text-white bg-green-500 hover:bg-green-600 hover:text-white px-4 py-1 rounded-md focus:outline-none transition-all duration-300 ease-in-out"
               >
                 Sign Up
               </Link>
