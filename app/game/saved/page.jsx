9aec668ceb80cbc832dd1e37150d2ba3258c9e52
alex <info@dndai.app>
2024-02-09 22:10:34 +0100

    fixed savedGames

--- app/game/saved/page.jsx
+++ app/game/saved/page.jsx
@@ -1,29 +1,34 @@
-import SaveGame from "@/components/Game/savegame/SaveGame";
-import React from "react";
-import { cookies } from "next/headers";
+'use client'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Cookies from "js-cookie"; // Import js-cookie for client-side cookie handling
+import SaveGame from "@/components/Game/savegame/SaveGame";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const getSavedGames = async () => {
-  const cookieStore = cookies();
+export default function Page() {
+  const [savedGames, setSavedGames] = useState([]);
 
-  const uid = cookieStore.get("uid").value;
+  useEffect(() => {
+    const getSavedGames = async () => {
+      const uid = Cookies.get("uid"); // Use js-cookie to get the cookie
 
-  try {
-    const response = await axios.get(BACKEND_URL + "/api/savedGames", {
-      params: {
-        _id: uid,
-      },
-    });
+      try {
+        const response = await axios.get(`${BACKEND_URL}/api/savedGames`, {
+          params: {
+            _id: uid,
+          },
+        });
 
-    return response.data;
-  } catch (error) {
-    return [];
-  }
-};
+        setSavedGames(response.data);
+      } catch (error) {
+        console.error("Failed to fetch saved games:", error);
+        setSavedGames([]);
+      }
+    };
 
-export default async function Page() {
-  const savedGames = await getSavedGames();
+    getSavedGames();
+  }, []); // The empty dependency array ensures this effect runs once on mount
+
   return <SaveGame data={savedGames} />;
 }
