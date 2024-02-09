33f80ea87ae957806d21e8204cf1eef5ed395e62
alex <info@dndai.app>
2024-02-09 21:15:44 +0100

    Fixed Gallery with usestate

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,34 +1,34 @@
-import Images from "@/components/images/Images";
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { cookies } from "next/headers";
+import Cookies from "js-cookie"; // Import js-cookie
+import Images from "@/components/images/Images";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+export default function Page() {
+  const [images, setImages] = useState([]);
 
-//fetch again on reload
+  useEffect(() => {
+    const getImages = async () => {
+      const uid = Cookies.get("uid"); // Use js-cookie to get the cookie
 
+      try {
+        // Make the API request to your backend with parameters correctly
+        const response = await axios.get(`${BACKEND_URL}/api/images/`, {
+          params: { _id: uid },
+        });
 
-const getImages = async () => {
-  const cookieStore = cookies();
+        // Update state with the fetched images
+        setImages(response.data.images);
+      } catch (error) {
+        console.error("Failed to fetch images:", error);
+        setImages([]);
+      }
+    };
 
-  const uid = cookieStore.get("uid").value;
-
-  try {
-    const response = await axios.get(BACKEND_URL + "/api/images/", {
-      params: {
-        _id: uid,
-      },
-    });
-
-    return response.data.images;
-  } catch (error) {
-    return [];
-  }
-};
-
-export default async function Page() {
-  const images = await getImages();
+    getImages();
+  }, []); // The empty dependency array ensures this effect runs once on mount
 
   return (
     <div>
