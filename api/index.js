c5daaea2504776550cede61c6cd3c212f2c91d89
alex <info@dndai.app>
2024-02-09 04:23:57 +0100

    removed cookies again from axois req

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api",
-  withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
   },
