a8677a3ed7bddb28b83ab1d45b2c05d5c0b512a0
alex <info@dndai.app>
2024-02-09 04:00:59 +0100

    middleware back

--- middleware.jsx
+++ middleware.jsx
@@ -1,34 +1,36 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export default function middleware(req) {
-  const url = req.nextUrl.clone(); // Clone the NextUrl object to manipulate
-  const clientLogin = req.cookies.get("uid");
+export default async function middleware(req) {
+  let url = req.url;
+  let baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  let clientLogin = req.cookies.get("uid")?.value;
 
-  // Paths that do not require authentication
-  const publicPaths = ['/login', '/register', '/'];
-
-  // Check if the current path is a public path
-  const isPublicPath = publicPaths.some(path => url.pathname.startsWith(path));
-
-  // Redirect logic for unauthenticated users trying to access protected routes
-  if (!clientLogin && !isPublicPath) {
-    url.pathname = '/'; // Redirect to home
-    return NextResponse.redirect(url);
+  //if not logged in and not on the login page
+  if (
+    !clientLogin &&
+    !url.includes("/login") &&
+    !url.endsWith("/") && url !== baseURL && !url.includes("/register")
+  ) {
+    console.log("here");
+    return NextResponse.redirect(`${baseURL}/`);
   }
 
-  // Redirect logic for authenticated users trying to access login or register
-  if (clientLogin && (url.pathname.includes("/login") || url.pathname.includes("/register"))) {
-    url.pathname = '/'; // Redirect to home
-    return NextResponse.redirect(url);
+  if (clientLogin && (url.includes("/login") || url.includes("/register"))) {
+    return NextResponse.redirect(`${baseURL}/`);
   }
-
-  // Proceed with the request for all other cases
-  return NextResponse.next();
 }
-
 export const config = {
   matcher: [
-    // Exclude specific paths from the middleware
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - robots.txt (SEO metadata file)
+     * - public (public files)
+     * - images (image files)
+     */
     "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|public|images|models|fonts|Audio|Logo|env|privacy|cookies|faq|imprint|instructions|terms|shop).*)",
   ],
 };
