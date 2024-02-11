9b8a933e7d8ef4cbb149cefd55d7417aac6c41e8
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-02-11 10:20:51 -0800

    added email verification and password reset

--- components/shared/BackgroundScene.jsx
+++ components/shared/BackgroundScene.jsx
@@ -6,10 +6,11 @@ import { Environment } from "@react-three/drei";
 import { Model } from "../Game/StartGame/Bird";
 import Loader from "../Game/StartGame/Loader";
 
+// BackgroundScene component
 export function BackgroundScene({ setLoaded }) {
   return (
-    <div className='fixed top-0 left-0 bg-black w-screen h-screen z-[3]'>
-      <Canvas className='z-10'>
+    <div className="fixed top-0 left-0 bg-black w-screen h-screen z-[3]">
+      <Canvas className="z-10">
         <Suspense fallback={<Loader />}>
           <Environment files={"/env/lebombo_1k.hdr"} />
           <Model setLoaded={setLoaded} />
