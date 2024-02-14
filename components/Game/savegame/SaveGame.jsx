54b021c52c844de539a8e7a1f68fa7b2e685a4f1
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-02-14 13:37:15 -0800

    added auth

--- components/Game/savegame/SaveGame.jsx
+++ components/Game/savegame/SaveGame.jsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import Loader from "@/components/shared/DragonLoader";
 
 const SaveGame = ({ data }) => {
+  //states
   const [userName, setUserName] = useState("");
   const { user } = useUserStore((state) => state);
   const { setIntroText } = useIntroTextStore((state) => state);
@@ -42,37 +43,37 @@ const SaveGame = ({ data }) => {
   };
 
   return (
-    <div className=' border  border-[#393a3b] rounded-md shadow-lg w-full sm:w-[70vw] mx-auto h-full min-h-screen  text-white overflow-hidden'>
+    <div className=" border  border-[#393a3b] rounded-md shadow-lg w-full sm:w-[70vw] mx-auto h-full min-h-screen  text-white overflow-hidden">
       {/* Header Section */}
-      <div className='relative flex flex-col items-center'>
+      <div className="relative flex flex-col items-center">
         <img
-          src='/dragonbaby.png'
-          alt='Header Image Alt Text'
-          className='w-full h-[77vh] object-cover mb-4 rounded-md'
+          src="/dragonbaby.png"
+          alt="Header Image Alt Text"
+          className="w-full h-[77vh] object-cover mb-4 rounded-md"
         />
 
         {/* Overlay */}
-        <div className='absolute inset-0 bg-black opacity-50 rounded-md'></div>
+        <div className="absolute inset-0 bg-black opacity-50 rounded-md"></div>
 
-        <h1 className='text-[2rem] sm:text-[3rem] text-center font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
+        <h1 className="text-[2rem] sm:text-[3rem] text-center font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
           Your Quest Awaits,{" "}
-          <span className='text-green-500 capitalize'>{userName}</span>!
+          <span className="text-green-500 capitalize">{userName}</span>!
         </h1>
       </div>
 
       <div>
-        <div className='bg-black p-4 sm:p-8'>
+        <div className="bg-black p-4 sm:p-8">
           {data?.length > 0 ? (
             data.map((savedGame, index) => {
               return (
                 <div
                   key={index}
-                  data-aos='fade-right'
-                  className='mb-4 border-l-4 border-green-500 pl-4'
+                  data-aos="fade-right"
+                  className="mb-4 border-l-4 border-green-500 pl-4"
                 >
                   <h2
                     onClick={() => handleClick(savedGame)}
-                    className='cursor-pointer hover:text-green-500 duration-300 transition-colors text-lg sm:text-xl font-bold mb-2'
+                    className="cursor-pointer hover:text-green-500 duration-300 transition-colors text-lg sm:text-xl font-bold mb-2"
                   >
                     {savedGame.title}
                   </h2>
@@ -82,13 +83,13 @@ const SaveGame = ({ data }) => {
               );
             })
           ) : (
-            <div className='flex flex-col w-full items-center pt-[20%] gap-y-2'>
-              <span className='opacity-60'>
+            <div className="flex flex-col w-full items-center pt-[20%] gap-y-2">
+              <span className="opacity-60">
                 No Saved Games yet, start your adventure{" "}
               </span>
               <button
                 onClick={startGame}
-                className='bg-gradient-to-t from-green-950 to-green-500 text-white px-4 py-2  rounded-md hover:to-green-700 hover:from-green-400  mb-2 sm:mb-2 hover:bg-green-600 focus:outline-none transition-colors duration-300'
+                className="bg-gradient-to-t from-green-950 to-green-500 text-white px-4 py-2  rounded-md hover:to-green-700 hover:from-green-400  mb-2 sm:mb-2 hover:bg-green-600 focus:outline-none transition-colors duration-300"
               >
                 Play Game
               </button>
