cc0fe32785beb37193ceabf559007f734e1423b3
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-02-25 12:56:02 -0800

    added new flow

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,6 @@ export default function Home() {
   const [loaded, setLoaded] = useState(false);
   const router = useRouter();
 
-  // States from your user store
   const { user } = useUserStore((state) => state);
   const cookies = new Cookies();
 
@@ -58,21 +57,21 @@ export default function Home() {
   }, [loaded]);
 
   return (
-    <div className='relative h-[85vh] bg-black z-[1]'>
+    <div className="relative h-[85vh] bg-black z-[1]">
       <BackgroundScene setLoaded={setLoaded} />
       {loaded && (
-        <div className='relative top-0 left-0 w-[100vw] h-full flex border justify-center items-end pb-32'>
-          <div className='w-[100vw] p-4 sm:p-8 mx-auto flex flex-col border justify-center items-center'>
-            <h1 className='text-xl neon-text sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 text-center text-white relative z-[14]'>
-              <span className='text-white'>
-                Welcome to <span className='text-green-500'>DnDAI</span>{" "}
+        <div className="relative top-0 left-0 w-[100vw] h-full flex border justify-center items-end pb-32">
+          <div className="w-[100vw] p-4 sm:p-8 mx-auto flex flex-col border justify-center items-center">
+            <h1 className="text-xl neon-text sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 text-center text-white relative z-[14]">
+              <span className="text-white">
+                Welcome to <span className="text-green-500">DnDAI</span>{" "}
                 Adventures!
               </span>
             </h1>
-            <div className='flex flex-col w-[80vw] sm:w-[20vw] z-[14]'>
+            <div className="flex flex-col w-[80vw] sm:w-[20vw] z-[14]">
               <button
                 onClick={startGame}
-                className='bg-gradient-to-t from-green-950 to-green-500 text-white px-6 py-2 mb-2 sm:mb-2 rounded-md hover:to-green-700 hover:from-green-400 transition-all'
+                className="bg-gradient-to-t from-green-950 to-green-500 text-white px-6 py-2 mb-2 sm:mb-2 rounded-md hover:to-green-700 hover:from-green-400 transition-all"
               >
                 Play Game
               </button>
