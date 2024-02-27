dae856a616121f7c6867faa9b9b64ccac9afb73c
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-02-27 14:11:21 -0800

    fixed ui revisions

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ import { logPageView } from "./../utils/analytics";
 import { getCredits } from "@/api/user";
 import { getGreenCredits } from "@/api/user";
 import { useSearchParams } from "next/navigation";
-import { initGA } from "./../utils/analytics";
+import { initGA } from "./../utils/analytics"; // Google Analytics
 export default function Home() {
   useEffect(() => {
     initGA(); // Initialize Google Analytics
@@ -54,21 +54,21 @@ export default function Home() {
   }, [loaded]);
 
   return (
-    <div className='relative h-[85vh] bg-black z-[1]'>
+    <div className="relative h-[85vh] bg-black z-[1]">
       <BackgroundScene setLoaded={setLoaded} />
       {loaded && (
-        <div className='relative top-0 left-0 w-[100vw] h-full flex border justify-center items-end pb-32'>
-          <div className='w-[100vw] p-4 sm:p-8 mx-auto flex flex-col border justify-center items-center'>
-            <h1 className='text-xl neon-text sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 text-center text-white relative z-[14]'>
-              <span className='text-white'>
-                Welcome to <span className='text-green-500'>DnDAI</span>{" "}
+        <div className="relative top-0 left-0 w-[100vw] h-full flex border justify-center items-end pb-32">
+          <div className="w-[100vw] p-4 sm:p-8 mx-auto flex flex-col border justify-center items-center">
+            <h1 className="text-xl neon-text sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 text-center text-white relative z-[14]">
+              <span className="text-white">
+                Welcome to <span className="text-green-500">DnDAI</span>{" "}
                 Adventures!
               </span>
             </h1>
-            <div className='flex flex-col w-[80vw] sm:w-[20vw] z-[14]'>
+            <div className="flex flex-col w-[80vw] sm:w-[20vw] z-[14]">
               <button
                 onClick={startGame}
-                className='bg-gradient-to-t from-green-950 to-green-500 text-white px-6 py-2 mb-2 sm:mb-2 rounded-md hover:to-green-700 hover:from-green-400 transition-all'
+                className="bg-gradient-to-t from-green-950 to-green-500 text-white px-6 py-2 mb-2 sm:mb-2 rounded-md hover:to-green-700 hover:from-green-400 transition-all"
               >
                 Play Game
               </button>
