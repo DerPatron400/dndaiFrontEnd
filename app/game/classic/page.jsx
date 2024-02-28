f498283369459cbb58f544249dbffaa2f3be63ff
alex <info@dndai.app>
2024-02-28 20:10:08 +0100

    added margin top for chat and added icon for generate image back

--- app/game/classic/page.jsx
+++ app/game/classic/page.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
 import dynamic from "next/dynamic";
 
 const GamePlay = dynamic(() => import("@/components/Game/Classic"), {
   ssr: false,
 });
-export default function page() {
+
+export default function Page() {
   return (
-    <div className='min-h-[80vh] mt-12'>
+    <div className='min-h-[90vh]' style={{ marginTop: '4%'}}>
       <GamePlay />
     </div>
   );
