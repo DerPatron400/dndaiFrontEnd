731b2cd086aae866f0f1ec363329cc8ac3dda717
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-04-24 05:47:58 -0700

    added responsiveness

--- components/landingPage/imagesVisual.jsx
+++ components/landingPage/imagesVisual.jsx
@@ -4,11 +4,11 @@ import Button from "../ui/custom-button";
 
 export default function imagesVisual() {
   return (
-    <div className=' w-full h-full flex flex-col justify-center items-center  text-white py-64'>
-      <h1 className='headline-1 text-center mb-16'>
+    <div className=" w-full h-full flex flex-col justify-center items-center  text-white md:py-64 py-32">
+      <h1 className="headline-1 text-center mb-16">
         AN INFINITE WORLD OF VISUALIZATION.
       </h1>
-      <div className='flex flex-col gap-6 mb-12 justify-center items-center w-full'>
+      <div className="flex flex-col gap-6 mb-12 justify-center items-center w-full">
         <Images speed={0.8} />
         <Images speed={0.6} />
       </div>
