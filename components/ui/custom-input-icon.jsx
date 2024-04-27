63296d8b19713f5663d8e77871c4f9aca95a61bc
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-04-27 13:34:52 -0700

    component section

--- components/ui/custom-input-icon.jsx
+++ components/ui/custom-input-icon.jsx
@@ -8,8 +8,8 @@ export default function CustomInputIcon({ placeholder, icon }) {
   const [value, setValue] = useState("");
 
   return (
-    <div className='relative h-[80px] running-text group'>
-      <div className='absolute inset-y-0 end-0 flex  items-center pe-3 pointer-events-none'>
+    <div className="relative h-[50px] w-96 running-text group">
+      <div className="absolute inset-y-0 end-0 flex  items-center pe-3 pointer-events-none">
         <IconButton
           className={cn(
             "bg-white/30 text-russianViolet transition-all duration-300 ease-in-out group ",
@@ -20,10 +20,10 @@ export default function CustomInputIcon({ placeholder, icon }) {
         </IconButton>
       </div>
       <input
-        type='text'
+        type="text"
         id={placeholder}
         value={value}
-        className='block w-full h-full  peer/input p-4 ps-[14px] pe-[20px]  text-sm placeholder:!text-sm placeholder:font-medium placeholder:opacity-100 text-white border border-gray2 rounded-lg bg-transparent hover:border-gray2 duration-300 transition-all focus:outline-0 focus:ring-offset-0 focus:ring-inset-irisPurpleLight focus:!ring-irisPurpleLight focus:!border-irisPurpleLight  placeholder:text-gray2 focus:shadow-lg focus:shadow-irisPurpleLight/10  '
+        className="block w-full h-full  peer/input p-4 ps-[14px] pe-[20px]  text-sm placeholder:!text-sm placeholder:font-medium placeholder:opacity-100 text-white border border-gray2 rounded-lg bg-transparent hover:border-gray2 duration-300 transition-all focus:outline-0 focus:ring-offset-0 focus:ring-inset-irisPurpleLight focus:!ring-irisPurpleLight focus:!border-irisPurpleLight  placeholder:text-gray2 focus:shadow-lg focus:shadow-irisPurpleLight/10  "
         placeholder={placeholder}
         onChange={(e) => setValue(e.target.value)}
         onFocus={() => setInFocus(true)}
