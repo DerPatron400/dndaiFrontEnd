aa1e16dce37a990804014afc670f8e2d7c8608c7
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-05-23 11:21:34 -0700

    added characters page

---
+++ app/character/mycharacter/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Characters from "@/components/myCharacter/characters";
+
+export default function page() {
+  return (
+    <div className="bg-russianViolet text-white">
+      <Characters />
+    </div>
+  );
+}
