1262e4621dea381928613d58d7c8ec9620d9db54
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-06-08 09:40:37 -0700

    changes

--- components/campaigns/createCampaign/Settings/Choose.jsx
+++ components/campaigns/createCampaign/Settings/Choose.jsx
@@ -4,32 +4,33 @@ import { SETTINGS } from "../constants";
 export default function Choose({ campaign, handleSetCampaign }) {
   return (
     <div
-      className='md:rounded-[16px] flex flex-col gap-5 w-full md:w-full
-     h-full md:p-5  md:pt-6 md:border md:border-white/10 overflow-auto hide-scrollbar'
+      className="md:rounded-[16px] flex flex-col gap-5 w-full md:w-full
+     h-full md:p-5  md:pt-6 pt-6 md:border md:border-white/10 overflow-auto hide-scrollbar"
     >
       {/* For PC */}
-      <h1 className='headline-4 hidden md:block'>Setting</h1>
+      <h1 className="headline-4 hidden md:block">Setting</h1>
+      <h1 className="headline-4 md:hidden block">Campaign Details</h1>
 
       {/* Ends */}
-      <div className='grid grid-cols-12 lg:grid-cols-12 gap-4 md:gap-2 w-full '>
+      <div className="grid grid-cols-12 lg:grid-cols-12 gap-4 md:gap-2 w-full ">
         {SETTINGS.map((setting, index) => (
           <div
             key={index}
             onClick={() => {
               handleSetCampaign("setting", setting);
             }}
-            className='col-span-2  cursor-pointer'
+            className="md:col-span-2 col-span-4  cursor-pointer"
           >
             <img
-              src='/images/setting/setting1.png'
-              alt='setting'
+              src="/images/setting/setting1.png"
+              alt="setting"
               className={cn(
                 `h-[135px] w-full ease-animate object-cover bg-white/10 rounded-[10px]`,
                 campaign?.setting === setting &&
                   "border-2 border-irisPurpleLight"
               )}
             />
-            <span className='description uppercase'>{setting}</span>
+            <span className="description uppercase">{setting}</span>
           </div>
         ))}
       </div>
--- components/campaigns/createCampaign/index.jsx
+++ components/campaigns/createCampaign/index.jsx
@@ -8,6 +8,8 @@ import { TIMES } from "./constants";
 import { createCampaign } from "@/actions/campaigns";
 import useUserStore from "@/utils/userStore";
 import { getCredits } from "@/actions/character";
+import CustomIconbutton from "@/components/ui/custom-iconbutton";
+
 const INITIAL_STATE = {
   title: "",
   time: TIMES[0],
@@ -19,6 +21,11 @@ export default function Index() {
   const [campaign, setCampaign] = useState(INITIAL_STATE);
   const [loading, setLoading] = useState(false);
   const { user, setYellowCredits, setBlueCredits } = useUserStore();
+  const [isSoundOn, setIsSoundOn] = useState(true);
+
+  function toggleSound() {
+    setIsSoundOn(!isSoundOn);
+  }
 
   const handleSetCampaign = (key, value) => {
     setCampaign((prev) => ({ ...prev, [key]: value }));
@@ -50,9 +57,9 @@ export default function Index() {
   };
 
   return (
-    <div className='h-screen max-h-screen w-full flex flex-col bg-gradient pt-[172px] md:pt-[120px] px-6 lg:px-12 md:pb-64 '>
-      <div className='hidden md:flex flex-col gap-2.5 '>
-        <h1 className='text-center flex justify-start text-white headline-3 z-[10] '>
+    <div className="h-screen max-h-screen w-full flex flex-col bg-gradient pt-[172px] md:pt-[120px] px-6 lg:px-12 md:pb-64  ">
+      <div className="hidden md:flex flex-col gap-2.5 ">
+        <h1 className="text-center flex justify-start text-white headline-3 z-[10] ">
           Create your own campaign
         </h1>
       </div>
@@ -61,22 +68,41 @@ export default function Index() {
           "flex flex-col gap-2.5 bg-blur-bottom-menu z-[12] w-screen left-0 h-[198px] px-5 pb-4 md:hidden fixed top-0 justify-end"
         }
       >
-        <h1 className='text-center flex justify-start text-white headline-3 z-[10] '>
+        <h1 className="text-center flex justify-start text-white headline-3 z-[10] ">
           Create your own campaign
         </h1>
       </div>
-      <div className='w-full h-[62vh] flex z-[10] '>
+      <div className="w-full md:h-[62vh] flex z-[10] ">
         <Create campaign={campaign} handleSetCampaign={handleSetCampaign} />
       </div>
-      <div className='flex justify-end items-end py-12 '>
+      <div className="md:flex justify-end items-end py-12 hidden ">
         <CustomButton
           variant={"primary"}
           disabled={!isValid() || loading}
           onClick={handleCreateCampaign}
         >
-          <img src='/Icons/Add.svg' alt='' className='h-5 w-5 opacity-75' />
-          Create campaign ( <img src='/gems/Legendary.png' alt='' /> 1)
+          <img src="/Icons/Add.svg" alt="" className="h-5 w-5 opacity-75" />
+          Create campaign ( <img src="/gems/Legendary.png" alt="" /> 1)
         </CustomButton>
+      </div>
+      <div className="z-[20] h-[88px] text-white fixed bottom-0 left-0 bg-russianViolet w-full flex  justify-center items-center py-4 md:hidden ">
+        <div className="flex justify-between items-center w-full p-4">
+          <CustomIconbutton onClick={toggleSound}>
+            <img
+              src={isSoundOn ? "/Icons/Sound.svg" : "/Icons/SoundOff.svg"}
+              alt="Sound Toggle"
+              className="h-5 w-5 invert"
+            />
+          </CustomIconbutton>
+          <CustomButton
+            variant={"primary"}
+            disabled={!isValid() || loading}
+            onClick={handleCreateCampaign}
+          >
+            <img src="/Icons/Add.svg" alt="" className="h-5 w-5 opacity-75" />
+            Create campaign
+          </CustomButton>
+        </div>
       </div>
     </div>
   );
