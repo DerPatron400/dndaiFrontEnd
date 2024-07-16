023a48aac68dad7fcc669da9cb0c1d5c04082fe9
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-07-16 02:19:00 -0700

    fixed icons

--- components/ui/Icons/Add.jsx
+++ components/ui/Icons/Add.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
+//add icon
 export default function Add(props) {
   return (
     <svg
-      data-name='Ebene 1'
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 20 20'
+      data-name="Ebene 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
       {...props}
     >
-      <path d='M17 11h-7v7H9v-7H2v-1h7V3h1v7h7v1Z' />
+      <path d="M17 11h-7v7H9v-7H2v-1h7V3h1v7h7v1Z" />
     </svg>
   );
 }
