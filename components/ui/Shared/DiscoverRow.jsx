04c228ea75ed2c99da87d4f263abf6f698384fca
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-07-25 06:43:23 -0700

    revisions

--- components/ui/Shared/DiscoverRow.jsx
+++ components/ui/Shared/DiscoverRow.jsx
@@ -3,11 +3,12 @@ import CampaignCarousel from "@/components/ui/Shared/Carousel/campaign";
 import CustomButton from "../custom-button";
 import ArrowRight from "../Icons/ArrowRight";
 import { useRouter } from "next/navigation";
+// import { useRouter } from "next/router";
 export default function row({ text, icon, campaigns, showMore }) {
   const router = useRouter();
   return (
-    <div className='flex flex-col gap-4 z-[10]'>
-      <div className='flex px-5 md:px-12 text-gray2 running-text-mono uppercase gap-2 items-center'>
+    <div className="flex flex-col gap-4 z-[10]">
+      <div className="flex px-5 md:px-12 text-gray2 running-text-mono uppercase gap-2 items-center">
         {icon}
         {text}
       </div>
@@ -20,7 +21,7 @@ export default function row({ text, icon, campaigns, showMore }) {
           className={"max-w-fit mx-auto mt-8"}
           onClick={() => router.push("/campaign/public")}
         >
-          Show More <ArrowRight className='h-5 w-5 fill-russianViolet' />
+          Show More <ArrowRight className="h-5 w-5 fill-russianViolet" />
         </CustomButton>
       )}
     </div>
