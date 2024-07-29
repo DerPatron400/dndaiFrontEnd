bdeaaba397534e2aae713a1ef231b6cedcc6d611
DerPatron400 <alexksela@hotmail.com>
2024-07-29 17:43:03 +0200

    fixed redirect

--- components/game/selectCharacter/index.jsx
+++ components/game/selectCharacter/index.jsx
@@ -10,12 +10,14 @@ export default function index({ characters }) {
   const router = useRouter();
 
   const handleRedirection = () => {
-    router.push("character/create");
+    router.push("/character/create");
   };
+
   console.log(currentCampaign);
+
   return (
-    <div className=' border-white gap-8 w-full flex flex-col  pt-[86px] md:pt-[128px] z-[10] text-white relative px-6 lg:px-12 md:pb-64 '>
-      <div className=' flex justify-center items-center relative  '>
+    <div className='border-white gap-8 w-full flex flex-col pt-[86px] md:pt-[128px] z-[10] text-white relative px-6 lg:px-12 md:pb-64'>
+      <div className='flex justify-center items-center relative'>
         <h1 className='headline-3 text-center w-3/4 md:w-1/2'>
           Begin your journey by selecting{" "}
           <span className='text-irisPurpleLight'>your hero!</span>
@@ -29,7 +31,7 @@ export default function index({ characters }) {
           Create character
         </CustomButton>
       </div>
-      <div className=' w-full grid  place-items-center grid-cols-12   gap-[20px]'>
+      <div className='w-full grid place-items-center grid-cols-12 gap-[20px]'>
         {characters.map((character, index) => (
           <Card loadGameOnClick={true} key={index} character={character} />
         ))}
