937c10769c94c67507c06fa299856bf95ecbe5f4
DerPatron400 <alexksela@hotmail.com>
2024-07-30 15:04:21 +0200

    fixed validation and password for signup / added publicEmailDomains json
    asd

--- components/auth/SignIn/signin.jsx
+++ components/auth/SignIn/signin.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useState } from "react";
 import CustomInput from "../../ui/custom-input";
 import CustomButton from "../../ui/custom-button";
@@ -11,6 +11,7 @@ import useCustomToast from "@/hooks/useCustomToast";
 import useUserStore from "@/utils/userStore";
 import Cookie from "universal-cookie";
 import { useRouter } from "next/navigation";
+
 export default function SignIn() {
   const cookies = new Cookie();
   const router = useRouter();
@@ -43,43 +44,47 @@ export default function SignIn() {
       setIsLoading(false);
     }
   };
+
+  const handleEmailChange = (value) => {
+    setEmail(value);
+    const isValid = validateEmail(value);
+    setIsEmailValid(isValid);
+  };
+
   return (
     <div
-      className='text-white w-[345px] md:h-auto mt-10 flex flex-col justify-between items-start gap-6 z-[9]'
+      className="text-white w-[345px] md:h-auto mt-10 flex flex-col justify-between items-start gap-6 z-[9]"
       suppressHydrationWarning
     >
-      <div className='md:flex flex-col gap-4 hidden '>
-        <h1 className='headline-3'>Sign in</h1>
-        <span className='text-gray2 running-text-small'>
+      <div className="md:flex flex-col gap-4 hidden ">
+        <h1 className="headline-3">Sign in</h1>
+        <span className="text-gray2 running-text-small">
           No account yet?{" "}
-          <Link className='text-white' href={"/auth/sign-up"}>
+          <Link className="text-white" href={"/auth/sign-up"}>
             Create an account
           </Link>
         </span>
       </div>
-      <div className='flex w-full flex-col gap-6 pt-[136px] md:pt-0'>
+      <div className="flex w-full flex-col gap-6 pt-[136px] md:pt-0">
         <CustomInput
-          placeholder='E-MAIL OR USERNAME'
+          placeholder="E-MAIL OR USERNAME"
           value={email}
-          onChange={(value) => {
-            setEmail(value);
-            validateEmail(value);
-          }}
+          onChange={(value) => handleEmailChange(value)}
           icon={
             isEmailValid && (
               <img
-                src='/Icons/Success.svg'
-                alt='Success'
-                className=' h-5 w-5'
+                src="/Icons/Success.svg"
+                alt="Success"
+                className=" h-5 w-5"
               />
             )
           }
-          className='relative' // Ensure the input field is positioned relative
+          className="relative" // Ensure the input field is positioned relative
         ></CustomInput>
 
-        <div className='gap-2.5 flex flex-col'>
+        <div className="gap-2.5 flex flex-col">
           <CustomInput
-            placeholder='PASSWORD'
+            placeholder="PASSWORD"
             value={password}
             onChange={(value) => setPassword(value)}
             type={isPasswordVisible ? "text" : "password"}
@@ -89,24 +94,24 @@ export default function SignIn() {
                 src={
                   isPasswordVisible ? "/Icons/Eye.svg" : "/Icons/EyeClosed.svg"
                 }
-                alt='Toggle Password Visibility'
-                className='h-5 w-5 invert cursor-pointer '
+                alt="Toggle Password Visibility"
+                className="h-5 w-5 invert cursor-pointer "
               />
             }
           />
           <Link
             href={"/auth/forgot-pass"}
-            className='text-white running-text-small cursor-pointer'
+            className="text-white running-text-small cursor-pointer"
           >
             Forgot password?
           </Link>
         </div>
       </div>
-      <div className='flex flex-col w-full gap-6'>
-        <div className='flex w-full justify-between items-center'>
-          <div className='flex justify-center items-center gap-2'>
-            <Checkbox className='border border-irisPurpleLight' />
-            <span className='text-white running-text-small text-center'>
+      <div className="flex flex-col w-full gap-6">
+        <div className="flex w-full justify-between items-center">
+          <div className="flex justify-center items-center gap-2">
+            <Checkbox className="border border-irisPurpleLight" />
+            <span className="text-white running-text-small text-center">
               Stay logged in
             </span>
           </div>
@@ -118,10 +123,10 @@ export default function SignIn() {
             {isLoading ? "Signing in..." : "Sign in"}
           </CustomButton>
         </div>
-        <div className='flex items-center justify-center w-full running-text-mono'>
-          <div className='border-t border-gray3 w-full'></div>
-          <div className='px-2 text-lg text-gray2'>OR</div>
-          <div className='border-t border-gray3 w-full'></div>
+        <div className="flex items-center justify-center w-full running-text-mono">
+          <div className="border-t border-gray3 w-full"></div>
+          <div className="px-2 text-lg text-gray2">OR</div>
+          <div className="border-t border-gray3 w-full"></div>
         </div>
         <GoogleAuth setIsLoading={setIsLoading} isLoading={isLoading} />
       </div>
