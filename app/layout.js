d34ed663fe3841b2a2203f2acc49cbdc629f3549
DerPatron400 <alexksela@hotmail.com>
2024-08-01 16:00:08 +0200

    changed pathTitles

--- app/layout.js
+++ app/layout.js
@@ -31,70 +31,55 @@ export default function RootLayout({ children }) {
   const characterSheet = pathname.includes("/character/sheet");
 
   useEffect(() => {
-    const initializeGtag = () => {
-      if (!window.gtag) return;
+    const setDocumentTitle = (url) => {
+      let title = "DnDAI - Play AI-Guided Pen and Paper Games"; // Default title
 
-      let title = pathname.split("/").pop().replaceAll("-", " ");
-
-      if (pathname.includes("character/sheet")) {
-        title = "Character Overview";
-      } else if (pathname === "/") {
-        title = "Home";
+      if (url.includes("character/sheet")) {
+        title = "Character Overview - DnDAI";
+      } else if (url === "/") {
+        title = "Home - DnDAI";
+      } else {
+        const pageTitle = url.split("/").pop().replaceAll("-", " ");
+        title = `${capitalizeFirstLetterOfEachWord(pageTitle)} - DnDAI`;
       }
-      const formattedPath =
-        pathname === "/" ? "home" : pathname.slice(1).replace("/", " / ");
-      document.title = title; // Set document.title to the URL
-      gtag("config", "G-BTHMYX7TZ9", {
-        page_title: document.title,
-        page_path: window.location.pathname,
-        screen_name: title, // Custom screen name
+
+      document.title = title;
+      window.gtag("config", "G-BTHMYX7TZ9", {
+        page_title: title,
+        page_path: url,
+        screen_name: title,
       });
     };
 
+    setDocumentTitle(pathname);
+
+    // Re-run on path changes
     const handleRouteChange = (url) => {
-      let title = url.split("/").pop().replaceAll("-", " ");
-
-      if (url.includes("character/sheet")) {
-        title = "Character Overview";
-      } else if (url === "/") {
-        title = "Home";
-      }
-
-      const formattedPath =
-        url === "/" ? "home" : url.slice(1).replace("/", " / ");
-      document.title = capitalizeFirstLetterOfEachWord(title); // Set document.title to the URL
-      window.gtag("config", "G-BTHMYX7TZ9", {
-        page_title: document.title,
-        page_path: url,
-        screen_name: title, // Custom screen name
-      });
+      setDocumentTitle(url);
     };
 
     if (window.gtag) {
-      initializeGtag(); // Capture the initial page load
+      handleRouteChange(pathname); // Capture the initial page load
     }
-
-    // Re-run on path changes
-    handleRouteChange(pathname);
   }, [pathname]);
 
   return (
-    <html lang='en' suppressHydrationWarning className={""}>
-      <GoogleOAuthProvider clientId='1036030324483-ltg0nqpg0ectr5q3n7cfa66l7eq1ban8.apps.googleusercontent.com'>
+    <html lang="en" suppressHydrationWarning className={""}>
+      <GoogleOAuthProvider clientId="1036030324483-ltg0nqpg0ectr5q3n7cfa66l7eq1ban8.apps.googleusercontent.com">
         <head>
           {/* Google Analytics Script */}
           <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1, maximum-scale=1'
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
           />
-          <link rel='icon' href='/favicon.ico' />
+          <link rel="icon" href="/favicon.ico" />
           <Script
-            strategy='afterInteractive'
+            strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=G-BTHMYX7TZ9`}
           />
           <Script
-            id='google-analytics'
-            strategy='afterInteractive'
+            id="google-analytics"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
@@ -107,10 +92,22 @@ export default function RootLayout({ children }) {
             }}
           />
           {/* Dynamic Meta Tags */}
-          <meta name="description" content="Join DnDAI to play AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free." />
-          <meta name="keywords" content="AI adventure games, text-based games, interactive fiction, role-playing games, free online adventure games" />
-          <meta property="og:title" content="Play AI-Guided Pen and Paper Games | DnDAI" />
-          <meta property="og:description" content="Join DnDAI to play AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free." />
+          <meta
+            name="description"
+            content="Join DnDAI to play AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free."
+          />
+          <meta
+            name="keywords"
+            content="AI adventure games, text-based games, interactive fiction, role-playing games, free online adventure games"
+          />
+          <meta
+            property="og:title"
+            content="Play AI-Guided Pen and Paper Games | DnDAI"
+          />
+          <meta
+            property="og:description"
+            content="Join DnDAI to play AI-supported pen and paper games. Explore adventure games, text-based games, and interactive fiction online for free."
+          />
           <meta property="og:url" content="https://www.dndai.app" />
           <meta property="og:type" content="website" />
         </head>
@@ -120,9 +117,9 @@ export default function RootLayout({ children }) {
           {!pathname.includes("components") && (
             <div>
               <img
-                src='/images/bg.png'
-                alt='Background'
-                className='h-screen w-screen object-fill fixed top-0 left-0 z-0'
+                src="/images/bg.png"
+                alt="Background"
+                className="h-screen w-screen object-fill fixed top-0 left-0 z-0"
               />
             </div>
           )}
@@ -131,13 +128,13 @@ export default function RootLayout({ children }) {
             characterSheet={characterSheet}
             variant={isTransparentNavbar ? "transparent" : "glass"}
           />
-          <div className='z-[1]'>{children}</div>
+          <div className="z-[1]">{children}</div>
 
           {showFooter && <Footer />}
           <Suspense fallback={null}>
             <CreditsDialogue />
           </Suspense>
-          <div className='!z-[50]'>
+          <div className="!z-[50]">
             <Toaster />
           </div>
         </body>
